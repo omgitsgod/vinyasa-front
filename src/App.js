23711d92c26dd430d3ebf2c2f8e321ef67545095
omgitsgod <omgitsgod@gmail.com>
2019-09-22 23:13:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import pose from './imgs/1.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="mat">
+          <img src={pose} className="App-logo" alt="logo" />
+          <p>
+            Vinyasa Flow
+          </p>
+        </div>
       </header>
     </div>
   );
