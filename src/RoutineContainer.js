ec9b6bffd1bce79ba34c54af2d0719ee2deb86da
omgitsgod <omgitsgod@gmail.com>
2019-09-22 23:25:08 -0400

    Add basic RoutineContainer file

---
+++ src/RoutineContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routine from './Routine'
+import './css/RoutineContainer.css';
+
+function RoutineContainer() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default RoutineContainer;
