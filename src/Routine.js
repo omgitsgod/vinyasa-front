b3bebbabde5382c1bfe026a6e677a1f4c53fda3a
omgitsgod <omgitsgod@gmail.com>
2019-09-25 14:05:12 -0400

    Add Time slider and name input

--- src/Routine.js
+++ src/Routine.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/Routine.css';
 
 function Routine(props) {
+  const [time, setTime] = useState(2)
+  useEffect(()=>{
+    props.addToRoutineTimes(props.num-1, 2)
+    return ()=> {
+      props.addToRoutineTimes(props.num-1, 0)
+    }
+  }, [])
   return (
-    <div>
+    <div className='pose'>
+    <div className='num'>
+      <p>Routine {props.num}</p>
+    </div>
+    <div className='name'>
+      <p>Name!</p>
+      <input />
+    </div>
+    <div className='time'>
+      <p>Time!</p>
+      <input onChange={
+        (e)=> {setTime(e.target.value); props.addToRoutineTimes(props.num-1, parseInt(e.target.value))}}
+        type="range" min={1} max={5} defaultValue={time} />
+      <p>{time} minute(s)</p>
+    </div>
     </div>
   );
 }
