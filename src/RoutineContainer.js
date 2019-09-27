42fa5ed72c960c696caa856ec85098e090a7b4d0
omgitsgod <omgitsgod@gmail.com>
2019-09-27 17:34:58 -0400

    Add calendar and save functionality

--- src/RoutineContainer.js
+++ src/RoutineContainer.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Calendar from 'react-calendar'
 import Routine from './Routine';
 import './css/RoutineContainer.css';
 
@@ -7,8 +8,18 @@ function RoutineContainer() {
   const [routineNum, setRoutineNum] = useState(1);
   const [timeLeft, setTimeLeft] = useState(60);
   const [routineTimes, setRoutineTimes] = useState([]);
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
 
-
+  useEffect(() => {
+    if (localStorage.getItem('routines')) {
+      setRoutines(JSON.parse(localStorage.getItem('routines')))
+      setRoutineNum(JSON.parse(localStorage.getItem('num')))
+    }
+    fetch(`https://vinyasa-backend.herokuapp.com/`)
+      .then(r => r.text())
+      .then(console.log)
+  },[])
   const addToRoutineTimes = (num, time) => {
     let temp = [...routineTimes]
     temp[num] = time
@@ -33,14 +44,27 @@ function RoutineContainer() {
     setRoutines([...routines, {num: routineNum}])
     setRoutineNum(routineNum + 1)
   }
+
   const deleteRoutine = () => {
     let num = routineNum - 1
     setRoutineNum(num)
     let temp = routines.filter(x => x.num !== num)
     setRoutines(temp)
   }
-  const displayRoutines = routines.map(x => <Routine key={x.num} num={x.num} addToRoutines={addToRoutines} addToRoutineTimes={addToRoutineTimes}/>)
+
+  const handleDate = (date) => {
+    setDate(date)
+    setOpen(false)
+  }
+
+  const db = () => {
+    fetch(`https://vinyasa-backend.herokuapp.com/getUser`,{method: 'GET', credentials: 'include'})
+  }
+
+  const displayRoutines = routines.map(x => <Routine load={x} key={x.num} num={x.num} addToRoutines={addToRoutines} addToRoutineTimes={addToRoutineTimes}/>)
+
   console.log(routines);
+
   return (
     <div className='mat'>
       <div className='heading'>
@@ -49,8 +73,17 @@ function RoutineContainer() {
           <button onClick={addRoutine}>+</button>
         </div>
         {timeLeft !== 1 ? <p>{timeLeft} Minutes</p> : <p>{timeLeft} Minute</p>}
+        {!open ? <p onClick={setOpen}>{date.toDateString()}</p> : <Calendar onChange={handleDate} value={date}/>}
       </div>
       {displayRoutines}
+      {routines.length > 0 ?
+      <div>
+      <button onClick={() => {setRoutines([]); localStorage.clear()}}>New</button>
+      <button onClick={() => {localStorage.setItem('routines', JSON.stringify(routines)); localStorage.setItem('num', routineNum)}}>Save</button>
+      </div>
+      :
+      null
+      }
     </div>
   );
 }
