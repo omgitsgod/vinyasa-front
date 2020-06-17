808803d87400179f3968b43de701eeba546a2bf8
Ryan <omgitsgod@gmail.com>
2020-06-17 14:16:10 -0400

    Fix formatting

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import Icon from '@mdi/react'
-import {mdiAccountCircle} from '@mdi/js'
-import GoogleButton from 'react-google-button'
-import RoutineContainer from './RoutineContainer'
-import pose from './imgs/25.svg'
+import Icon from '@mdi/react';
+import {mdiAccountCircle} from '@mdi/js';
+import GoogleButton from 'react-google-button';
+import RoutineContainer from './RoutineContainer';
+import pose from './imgs/25.svg';
 import './css/App.css';
 
 function App(props) {
-  const [user, setUser] = useState(null)
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [accountMenu, setAccountMenu] = useState(false)
+
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [accountMenu, setAccountMenu] = useState(false);
 
   const login = (x) => {
+
   //  props.history.push('/')
-    setUser(x)
-    setIsAuthenticated(true)
-    console.log('currently logged in as: ', x.name)
+    setUser(x);
+    setIsAuthenticated(true);
+    console.log('currently logged in as: ', x.name);
   }
+
   const logout = () => {
-    fetch(`https://vinyasa-backend.herokuapp.com/logout`,{method: 'GET',
-credentials: 'include'})
-    setAccountMenu(false)
-    setIsAuthenticated(false)
-    setUser(null)
+
+    fetch(`https://vinyasa-backend.herokuapp.com/logout`,{method: 'GET', credentials: 'include'});
+    setAccountMenu(false);
+    setIsAuthenticated(false);
+    setUser(null);
   }
 
   useEffect(() => {
+
     fetch(`https://vinyasa-backend.herokuapp.com/`,{method: 'GET', credentials: 'include'})
       .then(r => r.json())
       .then(json => {
         if (json.email){
         login(json)
       }
-      })
-  }, [])
+      });
+  }, []);
   return (
+    
     <div className="App">
       <div className="App-body">
           <img src={pose} className="App-logo" alt="logo" />
