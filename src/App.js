63f751591668c39bf1d9b940c7fbafedafcddcb8
Ryan <omgitsgod@gmail.com>
2020-07-12 20:42:59 -0400

    Refactor and fix formatting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Icon from '@mdi/react';
-import {mdiAccountCircle} from '@mdi/js';
+import { mdiAccountCircle } from '@mdi/js';
 import GoogleButton from 'react-google-button';
 import RoutineContainer from './RoutineContainer';
 import pose from './imgs/25.svg';
 import './css/App.css';
+
 
 function App(props) {
 
   const [user, setUser] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [accountMenu, setAccountMenu] = useState(false);
+  const { history } = props;
 
   const login = (x) => {
 
-  //  props.history.push('/')
+    //history.push('/')
     setUser(x);
     setIsAuthenticated(true);
     console.log('currently logged in as: ', x.name);
+  }
+
+  const handleLogin = async () => {
+
+    const user = await fetch(`https://vinyasa-backend.herokuapp.com/`, {
+      method: 'GET',
+      credentials: 'include',
+    }).then((r) => r.json());
+
+    if (user.email) {
+      login(user);
+    }
   }
 
   const logout = () => {
@@ -30,30 +44,39 @@ function App(props) {
 
   useEffect(() => {
 
-    fetch(`https://vinyasa-backend.herokuapp.com/`,{method: 'GET', credentials: 'include'})
-      .then(r => r.json())
-      .then(json => {
-        if (json.email){
-        login(json)
-      }
-      });
+    handleLogin();
   }, []);
+
   return (
-    
-    <div className="App">
-      <div className="App-body">
-          <img src={pose} className="App-logo" alt="logo" />
-          <RoutineContainer isAuthenticated={isAuthenticated} user={user}/>
+    <div className='App'>
+      <div className='App-body'>
+          <img src={pose} className='App-logo' alt='logo' />
+          <RoutineContainer isAuthenticated={isAuthenticated} user={user} />
           {isAuthenticated ?
-            <img className='avatar' src={user.photo} alt={'avatar'} onClick={()=>setAccountMenu(!accountMenu)}/>
-            :
-              <Icon path={mdiAccountCircle}
-                size={2}
-                color='#92a3a8'
-                onClick={() => setAccountMenu(!accountMenu)}
-              />
+            <img className='avatar' src={user.photo} alt={'avatar'} onClick={()=>setAccountMenu(!accountMenu)} />
+          :
+            <Icon path={mdiAccountCircle}
+              size={2}
+              color='#92a3a8'
+              onClick={() => setAccountMenu(!accountMenu)}
+            />
           }
-          {accountMenu ? !isAuthenticated ? <div><br/><a href="https://vinyasa-backend.herokuapp.com/auth/google"><GoogleButton/></a></div> : <div><br/><button onClick={logout}>Sign out {user.name}</button></div> : null}
+          {accountMenu ? 
+            !isAuthenticated ? 
+              <div>
+                <br />
+                <a href='https://vinyasa-backend.herokuapp.com/auth/google'><GoogleButton /></a>
+              </div> 
+            : 
+              <div>
+                <br />
+                <button onClick={logout}>
+                  Sign out {user.name}
+                </button>
+              </div> 
+          : 
+            null
+          }
           <p>
             Vinyasa Flow
           </p>
