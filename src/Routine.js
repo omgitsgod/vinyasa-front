9c96cec51e8fc1f798e0041b628800206f451de7
Ryan <omgitsgod@gmail.com>
2020-07-12 20:43:14 -0400

    Refactor and fix formatting

--- src/Routine.js
+++ src/Routine.js
@@ -1,46 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { poses } from './constants';
+import { poses, svgs } from './constants';
 import './css/Routine.css';
 
-const reqSvgs = require.context('./imgs', true, /\.svg$/);
-const paths = reqSvgs.keys();
-const svgs = paths.reduce((images,path) => {
-  images[path] = reqSvgs(path);
-  return images;
-  }, {});
 
 function Routine(props) {
 
   const [time, setTime] = useState(2);
   const [pose, setPose] = useState({});
   const [open, setOpen] = useState(true);
+  const { handleSessionTime, load, addToRoutines, num, routines } = props;
 
-//  useEffect(()=>{
-//    props.addToRoutineTimes()
-//    if (props.load.id) {
-//      setPose(props.load)
+  useEffect(()=>{
+
+    handleSessionTime()
+//    if (load.id) {
+//      setPose(load)
 //      setOpen(false)
-//      props.addToRoutines(props.num-1, props.load)
+//      addToRoutines(num-1, load)
 //   }
-//    return ()=> {
-//      props.addToRoutineTimes([...props.routines].filter(x => x.num !== props.num))
-//    }
-//  }, [props])
+    return ()=> {
+     handleSessionTime([...routines].filter(x => x.num !== num))
+    }
+  }, [handleSessionTime])
 
   const select = (e) => {
 
-    let term = e.target.value;
+    const term = e.target.value;
+    let tempPose;
 
-    if(term > 0) {
-      let temp = pose;
-      temp.time = parseInt(term);
-      setTime(temp.time);
-      setPose(temp);
-      props.addToRoutines(props.num-1, temp);
+    if (term > 0) {
+      tempPose = pose;
+      tempPose.time = parseInt(term);
+      setTime(tempPose.time);
+      setPose(tempPose);
+      addToRoutines(num-1, tempPose);
     } else {
       if (poses.filter(x => x.english_name === term)[0] || poses.filter(x => x.sanskrit_name === term)[0]) {
-        let tempPose;
-
         if (poses.filter(x => x.english_name === term)[0]) {
           tempPose = {...poses.filter(x => x.english_name === term)[0]};
           tempPose.chosen = tempPose.english_name;
@@ -52,7 +47,7 @@ function Routine(props) {
         tempPose.time = time;
         setPose(tempPose);
         setOpen(false);
-        props.addToRoutines(props.num-1, tempPose);
+        addToRoutines(num-1, tempPose);
       }
     }
   }
@@ -60,21 +55,50 @@ function Routine(props) {
   return (
     <div className='pose'>
       <div className='name'>
-        {pose.chosen && !open ? <p className='name-text' onClick={() => setOpen(!open)}>{pose.chosen}</p> : null}
+        {pose.chosen && !open ? 
+          <p className='name-text' onClick={() => setOpen(!open)}>
+            {pose.chosen}
+          </p> 
+        : 
+          null
+        }
         {open ?
           <div>
-          <input list='brow' className='search' onChange={select}/>
-        <datalist id='brow'>
-          {[...poses.map(x => <option key={x.id} value={x.english_name}/>),...poses.map(x => <option key={x.id + 48} value={x.sanskrit_name}/>)]}
-        </datalist>
-        </div>
-        : null }
-        {pose.id && !open ? <img src={svgs[pose.img]} className='pose-img' alt='pose'/> : null}
+            <input list='brow' className='search' onChange={select} />
+            <datalist id='brow'>
+              {[
+                ...poses.map(x => <option key={x.id} value={x.english_name} />),
+                ...poses.map(x => <option key={x.id + 48} value={x.sanskrit_name} />)
+              ]}
+            </datalist>
+          </div>
+        : 
+          null 
+        }
+        {pose.id && !open ? 
+          <img src={svgs[pose.img]} className='pose-img' alt='pose' /> 
+        : 
+          null
+        }
       </div>
       <div className='time'>
-        <input className='slider' onChange={select}
-          type="range" min={1} max={5} defaultValue={props.load.time || props.time} />
-        {time !== 1 ? <p className='time-text'>{props.load.time || time} minutes</p> : <p className='time-text'>{props.load.time || time} minute</p>}
+        <input 
+          className='slider'
+          onChange={select}
+          type='range'
+          min={1}
+          max={5}
+          defaultValue={load.time || time} 
+        />
+        {time !== 1 ? 
+          <p className='time-text'>
+            {load.time || time} minutes
+          </p> 
+        : 
+          <p className='time-text'>
+            {load.time || time} minute
+          </p>
+        }
       </div>
     </div>
   );
